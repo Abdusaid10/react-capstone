d1f016f619ddda4ed8956a008057074fc064e593
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-08-19 20:37:44 +0500

    change Area component

--- src/components/Area.js
+++ src/components/Area.js
@@ -1,69 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Area = ({ areaFilter }) => {
-  const areasList = [
-    'British',
-    'Canadian',
-    'Chinese',
-    'Dutch',
-    'Egyptian',
-    'French',
-    'Greek',
-    'Indian',
-    'Irish',
-    'Italian',
-    'Jamaican',
-    'Japanese',
-    'Kenyan',
-    'Malaysian',
-    'Mexican',
-    'Moroccan',
-    'Polish',
-    'Russian',
-    'Spanish',
-    'Thai',
-    'Tunisian',
-    'Turkish',
-    'Unknown',
-    'Vietnamese',
-  ];
-
-  const handleChange = (name) => {
-    areaFilter(name);
+const Area = ({ name, clickHandlerArea }) => {
+  
+  const handleClickArea = (name) => {
+    clickHandlerArea(name);
   }
 
   return (
-    // <select name="area" onChange={handleChange}>
-    //   <option key="All" value="American" defaultValue>
-    //     American
-    //   </option>
-    //   {areasList.map(area => (
-    //     <option value={area} key={area}>{area}</option>
-    //   ))}
-    // </select>
     <div className="areas-container">
-      <h3>
-        Select an Area 
-      </h3>
-      {/* {areasList.map(area => (
-        <Area 
-          key={area}
-          area={area}
-          areaFilter={aFilter => handleClickArea(aFilter)
-          } />
-      ))} */}
-      { areasList.map(area => (
-        <button type="button" key={area} name={area} onClick={() => handleChange(area)}>{area}</button>
-      ))
-      }
+      <button key={name} type="button" name={name} onClick={() => handleClickArea(name)}>{name}</button>
     </div>
   );
 }
 
 Area.propTypes = {
-  area: PropTypes.string,
-  areaFilter: PropTypes.func,
+  name: PropTypes.string,
+  areaFclickHandlerAreailter: PropTypes.func,
 };
 
 export default Area;
