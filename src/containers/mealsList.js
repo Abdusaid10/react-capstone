298f8853bc048c50a8bc4aa3b11114b5d9aff81e
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-08-19 20:37:53 +0500

    modify the mealsList to make proper fetch on change of the area

--- src/containers/mealsList.js
+++ src/containers/mealsList.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Meal from '../components/Meal';
 import Area from '../components/Area';
 import { fetchMealsByArea, fetchMealsByCategory, fetchMealsByIngredient } from '../actions/index';
-import { onChangeArea } from '../actions/filterAction';
+import { changeArea } from '../actions/filterAction';
 /* 
   filters = filter {
     if (filter == area ){
@@ -17,7 +17,7 @@ import { onChangeArea } from '../actions/filterAction';
   search by ingredient
 */
 
-const MealsList = ({ meals, fetchMealsByArea, aFilter, onChangeArea }) => {
+const MealsList = ({ meals, areaFilter, fetchMealsByArea, changeArea }) => {
   const areasList = [
     'British',
     'Canadian',
@@ -44,9 +44,10 @@ const MealsList = ({ meals, fetchMealsByArea, aFilter, onChangeArea }) => {
     'Unknown',
     'Vietnamese',
   ];
+  
   const [currentMeal, setCurrentMeal] = useState([]);
   // const [currentArea, setCurrentArea] = useState([]);
-
+  // [area] = useState([]);
   const handleClick = meal => {
     alert(meal.strMeal);
     setCurrentMeal(meal.strMeal);
@@ -57,14 +58,25 @@ const MealsList = ({ meals, fetchMealsByArea, aFilter, onChangeArea }) => {
   //   onChangeArea(area);
   // };
 
-  useEffect(() => {
-    fetchMealsByArea(currentArea);
-    // onChangeArea(currentArea);
-  }, []);
+  // useEffect(() => {
+  //   fetchMealsByArea(area);
+  //   // onChangeArea(currentArea);
+  // }, [area, fetchMealsByArea]);
 
   return (
     <div className="container">
-      <Area areaFilter={filter => onChangeArea(filter)} />
+      <div className="areas-container">
+        <h3>
+          Select an Area 
+        </h3>
+          {areasList.map(area => (
+            <Area key={area} name={area} clickHandlerArea={(area) => {
+              changeArea(area);
+              // setCurrentArea(area);
+              fetchMealsByArea(area);
+              }} />
+          ))}
+      </div>
       <div className="meals-container">
           {meals
             .map((meal) => (
@@ -77,12 +89,14 @@ const MealsList = ({ meals, fetchMealsByArea, aFilter, onChangeArea }) => {
 
 const mapStateToProps = state => ({
   meals: state.mealsList.meals,
-  aFilter: state.aFilter,
+  area: state.areaFilter,
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchMealsByArea: aFilter => dispatch(fetchMealsByArea(aFilter)),
-  onChangeArea: () => dispatch(onChangeArea()),
+  // fetchMealsByArea: aFilter => dispatch(fetchMealsByArea(aFilter)),
+  // onChangeArea: () => dispatch(onChangeArea()),
+  changeArea: area => dispatch(changeArea(area)),
+  fetchMealsByArea: (area) => dispatch(fetchMealsByArea(area)),
 })
 
 
@@ -94,15 +108,15 @@ MealsList.propTypes = {
       idMeal: PropTypes.number.isRequired,
     }),
   ).isRequired,
-  activeMeal: PropTypes.bool,
-  aFilter: PropTypes.string,
-  clickHandler: PropTypes.func,
-  fetchMealsByArea: PropTypes.func,
-  onChangeArea: PropTypes.func,
+  loading: PropTypes.bool,
+  error: PropTypes.string,
+  area: PropTypes.string,
+  changeArea: PropTypes.func,
+  fetchMealsByArea: PropTypes.func.isRequired,
 };
 
-MealsList.defaultProps = {
-  activeMeal: false,
-};
+// MealsList.defaultProps = {
+//   activeMeal: false,
+// };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MealsList);
